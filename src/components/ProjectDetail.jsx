31299729293111939dec324cf1e2c2b369c1353b
Jorge Marcos <jorge@lgcr.co>
2026-09-14 16:16:15 -0600

    labels

--- src/components/ProjectDetail.jsx
+++ src/components/ProjectDetail.jsx
@@ -218,7 +218,7 @@ const ProjectDetail = ({ proyecto }) => {
                     text-neutral-500
                   "
                 >
-                  {stat.etiqueta}
+                  {stat.label}
                 </div>
               </div>
             ))}
